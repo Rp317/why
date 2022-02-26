f416d104702726fae5796a18c08d05a0522d2ef9
Rp317 <purajarikrussell@gmail.com>
2022-02-26 09:55:27 -0600

    Initial commit

---
+++ Hello.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function yes() {
+  let items = document.getElementById('classes');
+  let stuff = document.createElement('button');
+  stuff.classList.add('classeses');
+  items.appendChild(stuff);
+}
+export default function Hello() {
+  let owo = document.getElementById("owo");
+  owo.style.display = "block";
+  document.getElementById("joe").style.display = 'none';
+}
